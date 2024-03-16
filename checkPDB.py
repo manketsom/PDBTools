fce6674a173a6ca6d3d6a99853e3b5ef086da344
manketsom <busisiwemangena23@gmail.com>
2024-03-16 11:26:44 +0200

    Fixed my conditions for retrieving protein residues for a chain. Had to make adjustsments for residues that are not available.

--- checkPDB.py
+++ checkPDB.py
@@ -28,8 +28,8 @@ while True:
     if PDB_ID == "q" or PDB_ID == "quit" or PDB_ID == "Q":
         break
     # User must only input a PDB_ID with upper case.
-    if PDB_ID.islower():
-        print("Error. Please use uppercase for all characters.")
+    if not PDB_ID.isupper():
+        print("Error.Please use uppercase for all characters.")
         continue
     # Download the PDB file that is given to the input.
     try:
@@ -51,6 +51,9 @@ while True:
         # Loop will break.
         if option == "q" or option == "Q" or option == "quit":
             break
+        if not option.isnumeric():
+            print("Error. Option has to be numeric")
+            continue
         # Adding a try-exception block in order to catch errors.
         try:
             # This is what will be printed out depending on the user's option.
@@ -92,24 +95,24 @@ while True:
         # Loop will break.
         if chain == "q" or chain == "quit" or chain == "Q":
             break
+        elif not chain.isalpha() or not chain.isupper():
+            print("Error. Input has to be single uppercase letter")
+            continue
         if output_filename == "q" or output_filename == "Q" or output_filename == "quit":
             break
         # Adding a try-exception block here.
         try:
             # This is what will be printed out.
-            if chain == "A":
+            if chain == "A" or chain == "B":
                 residues = module.protein_residues(PDB_ID, chain)
                 result = module.fasta_file(PDB_ID,chain,output_filename)
-                print(residues)
-                print("Residues have been written to your file")
-            elif chain == "B":
-                residues = module.protein_residues(PDB_ID, chain)
-                result = module.fasta_file(PDB_ID,chain, output_filename)
-                print(residues)
-                print("Residues have been written to your file")
+                if residues:
+                    print(residues)
+                    print("Residues have been written to your file")
+                else:
+                print("Error.Protein residues for chain {} are not available".format(chain))
             else:
-                print("Error.Given input is not in option menu.Please refer to the option list.")
-                print("Output_filename will not be written into")
+                print("Error. Given input is invalid. Please refer to the option list.")
         except ValueError:
             print("Error. Please input a valid chain ID/output_filename")
 
@@ -119,12 +122,19 @@ while True:
         chain = input("To retreive the relevant lines of the PDB file, please firstly provide a chain ID of your choice:")
         if chain == "q" or chain == "Q" or chain == "quit":
             break
+        elif not chain.isalpha() or not chain.isupper():
+            print("Error.Chain_ID has to be in uppercase")
         record_type = input("Secondly, please provide the record type (ATOM or HETATM):")
         # Loop will break
         if record_type == "q" or record_type == "Q" or record_type == "quit":
             break
+        if not record_type.isalpha() or not record_type.isupper():
+            print("Error.Record_type has to be in uppercase.")
         # This is where the user will specify if they want to print the lines or write them to a file.
         output = input("What would you like to do with the lines? Write them to a file (W) or read them (R)?")
+        if not output.isupper() or not output.isalpha():
+            print("Error.Output has to a single uppercase letter")
+            continue
         if output == "R":
             if chain in ["A","B"] and record_type in ["ATOM","HETATM"]:
                     lines = module.relevant_lines(PDB_ID, chain, record_type)
@@ -148,6 +158,9 @@ while True:
         chain = input("To retreive the non-standard protein residues of a particular chain, please input a chain ID:")
         if chain == "q" or chain == "Q" or chain == "quit":
             break
+        if not chain.isupper() or not chain.isalpha():
+            print("Error.Chain_ID has to be in uppercase")
+            continue
         try:
             
             if chain in ["A","B"]:
@@ -155,7 +168,9 @@ while True:
                 print(non_standard_residues)
 
             else:
-                print("Error. Please provide a chain ID")
+                print("Error.Please provide a chain ID")
+                if non_standard_residues is None:
+                    print("Non_standard_residues for chain {} are unavailable.".format(chain))
         except ValueError:
             print("Error.Please input correct chain ID (A or B)")
 
@@ -166,7 +181,12 @@ while True:
             input_file = input("Please input the name of the pdb_file you have downloaded:")
             output_file = input("Please input the name you want to give to the output file:")
             chain = input("What chain are you wanting to alter:")
+            if not chain.isalpha() or not chain.isupper():
+                print("Error. Chain_ID has to uppercase")
+                continue
             new_chain = input("What do you want to rename the chain to?:")
+            if not new_chain.isalpha() or not new_chain.isupper():
+                print("Error.new_chain needs to be uppercase")
             # Loop will break:
             if input_file == "Q" or input_file == "quit" or input_file == "q":
                 break
@@ -193,11 +213,17 @@ while True:
             print("The following prompts will allow you to get a plot of the temperature factors of a protein given a chain ID")
             # Ask the user for inputs.
             chain = input("Please input a chain ID:")
+            if not chain.isalpha() or not chain.isupper():
+                print("Error.Chain_ID has to be uppercase")
+                continue
             # Breaking the loop.
             if chain == "Q" or chain == "quit" or chain == "q":
                 break
             # Ask the user for plot_dimensions
             plot_dimensions = input("Please put the dimensions you want the plot to have (Enclose them in round brackets):")
+            if not plot_dimensions.isnumeric():
+                print("Error.Plot_dimensions have to be numerical")
+                continue
             # Breaking the loop
             if plot_dimensions == "Q" or plot_dimensions == "quit" or plot_dimensions == "q":
                 break
